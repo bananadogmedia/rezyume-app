29b234b657fa32cb13fcf5ad038274778bb8b28b
BananaDog Media <info@bananadogmedia.com>
2022-06-30 16:51:14 -0700

    Updated Test

--- src/test_data.js
+++ src/test_data.js
@@ -1,13 +1,13 @@
 //Data used when the user hits "Example" btn
 const data = {
   contact: {
-    name: "Dimitry A. Berg",
+    name: "Damien Bacardi",
     address: "2700 Las Vegas Blvd S,",
     city: "Las Vegas,",
     state: "NV",
     zip: "89109,",
-    phone: "515-451-9383",
-    email: "dimitryberg@mail.com",
+    phone: "515-555-9383",
+    email: "damienbacardi@mail.com",
     summary:
       "Ful-Stack Developer with 15+ years of hands-on experience designing, developing, and implementing applications and solutions using a range of technologies and programming languages. Seeking to leverage broad development experience in a challenging, fast and forward-thinking role.",
   },
@@ -21,13 +21,13 @@ const data = {
       "Developed an award-winning creative agency with 60+clients, located in Las Vegas/Los Angeles specializing in Web Development/Design, Digital Marketing, Branding and Identity.",
       "Planned, designed and executed 100+ succesful projects utilizing a wide range of technologies and programming languages.",
     ],
-    company2: "Practice Owl LLC",
-    local2: "Camas, WA 98607, USA",
+    company2: "Awesome Company",
+    local2: "Camas, WA 98707, USA",
     position2: "Full Stack Developer",
     start2: "October 2019",
     end2: "- Present",
     desc2: [
-      "Designed and developed a first of its kind innovative patient education framework, interface and web app to deliver doctor patient education.",
+      "Designed and developed a first of its kind innovative education framework, interface and web app to deliver educational content.",
       "Wrote 800,000+ lines of frontend and backend code utilizing the .NET Core framework, RESTful API and modern HTML, CSS and Javascript.",
     ],
   },
